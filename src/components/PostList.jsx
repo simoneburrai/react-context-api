fa37339053a0d1e1890ebc7aeac39ba9e0f2846a
simoneburrai <burrai.s98@gmail.com>
2025-04-23 18:52:14 +0200

    feat: imported useContext in post list, painted in page list of the posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,14 @@
+import { useContext } from "react";
+import PostContext from "../contexts/PostContext";
+
 const PostList = () => {
+    const posts = useContext(PostContext);
+    console.log(posts);
+
     return <div className="post-container">
-        Lista dei post (PostList)
+        {posts.map(post => {
+            return <div className="post" id={post.id}>{post.title}</div>
+        })}
     </div>
 }
 
--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -2,4 +2,6 @@ import { createContext } from "react";
 
 const PostContext = createContext();
 
+const PostProvider = () => 
+
 export default PostContext;
